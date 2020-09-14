53de168d72acd29724ff78474e0b233adb3b95ce
dewerseb <bastien.dewerse@gmail.com>
2020-09-14 15:57:43 +0200

    dashboard style

--- view/pages/dashboard.php
+++ view/pages/dashboard.php
@@ -11,67 +11,33 @@
 ?>
 
 <h2>Dernières factures:</h2>
-<!-- <table>
+<table>
     <th>#ID</th>
     <th>Numéro de facture</th>
     <th>Date</th>
     <th>Contact</th>
     <th>Société</th>
-    <th>Détails</th>
-    <th>Modifier</th>
-    <th>Supprimer</th>
+    <th>Gérer</th>
     <?php
-       $invoicesList = $model->data;
-       for ($i=0; $i < 4; $i++) { 
-           $invoice = $invoicesList[$i];
-           echo "<tr>";
+       $invoicesList = $model->data[1];
+       foreach ($invoicesList as $invoice) {
+            echo "<tr>";
             echo "<td>".$invoice['invoice_id'].  "</td>";
             echo "<td>".$invoice['invoice_number'].  "</td>";
             echo "<td>".$invoice['date'].  "</td>";
-            echo "<td>".$invoice['contact_id'].  "</td>";
-            echo "<td>".$invoice['company_id'].  "</td>";
-            echo "<td> <a href = \"/COGIP-app/invoices/details/$invoice[invoice_id]\"><button>Détails</button><a></td>";
+            echo "<td>".$invoice['lastname'].  "</td>";
+            echo "<td>".$invoice['name'].  "</td>";
+            echo "<td><a href = \"/COGIP-app/invoices/details/$invoice[invoice_id]\"><button class='manage'><i class='fas fa-external-link-square-alt'></i></button><a>";
             if (Auth::isLogged() && $_SESSION['usertype'] === 'admin') { 
-                echo "<td> <a href = \"/COGIP-app/invoices/update/$invoice[invoice_id]\"><button>Modifier</button><a></td>";
-                echo "<td> <a href = \"/COGIP-app/invoices/delete/$invoice[invoice_id]\"><button>Supprimer</button><a></td>";
+                echo "<a href = \"/COGIP-app/invoices/update/$invoice[invoice_id]\"><button class='manage'><i class='fas fa-pen-square'></i></button><a>";
+                echo "<a href = \"/COGIP-app/invoices/delete/$invoice[invoice_id]\"><button class='manage'><i class='fas fa-minus-square'></i></button><a>";
             }
-           echo "</tr>";
+            echo "</td></tr>";
 
        }
     ?>
 </table>   
 
-<h2>Dernier contacts:</h2>
-<table>
-    <th>ID</th>
-    <th>Prénom</th>
-    <th>Nom</th>
-    <th>Email</th>
-    <th>Téléphone</th>
-    <th>Société</th>
-    <th>Détails</th>
-    <th>Modifier</th>
-    <th>Supprimer</th>
-
-    <?php
-       $contactList = $model->data;
-       for ($i=0; $i < 4; $i++) { 
-           $contact = $contactList[$i];
-           echo "<tr>";
-            echo "<td>".$contact['contact_id'].  "</td>";
-            echo "<td>".$contact['firstname'].  "</td>";
-            echo "<td>".$contact['lastname'].  "</td>";
-            echo "<td>".$contact['email'].  "</td>";
-            echo "<td>".$contact['phone'].  "</td>";
-            echo "<td>".$contact['company_id'].  "</td>";
-            echo "<td> <button><a href = \"/COGIP-app/contact/details/$contact[contact_id]\">Détails<a></button></td>";
-            echo "<td> <button><a href = \"/COGIP-app/contact/update/$contact[contact_id]\">Modifier<a></button></td>";
-            echo "<td> <button><a href = \"/COGIP-app/contact/delete/$contact[contact_id]\">delete<a></button></td>";
-           echo "</tr>";
-
-       }
-    ?>
-</table>    -->
 <h2>Dernières sociétés:</h2>
 <table>
     <th>#ID</th>
@@ -79,19 +45,53 @@
     <th>Numéro de TVA</th>
     <th>Pays</th>
     <th>type</th>
-
+    <th>Gérer</th>
     <?php
-       $companyList = $model->data;
-       $company = $companyList[0];
-       for ($i=0; $i < 5; $i++) { 
-           echo "<tr>";
-            echo "<td>".$company[$i]['company_id'].  "</td>";
-            echo "<td>".$company[$i]['name'].  "</td>";
-            echo "<td>".$company[$i]['VAT'].  "</td>";
-            echo "<td>".$company[$i]['country'].  "</td>";
-            echo "<td>".$company[$i]['company_type'].  "</td>";
-           echo "</tr>";
+       $companiesList = $model->data[0];
+       foreach ($companiesList as $company) { 
+            echo "<tr>";
+            echo "<td>".$company['company_id'].  "</td>";
+            echo "<td>".$company['name'].  "</td>";
+            echo "<td>".$company['VAT'].  "</td>";
+            echo "<td>".$company['country'].  "</td>";
+            echo "<td>".$company['company_type'].  "</td>";
+            echo "<td><a href = \"/COGIP-app/companies/details/$company[company_id]\"><button class='manage'><i class='fas fa-external-link-square-alt'></i></button><a>";
+            if (Auth::isLogged() && $_SESSION['usertype'] === 'admin') {
+                echo "<a href = \"/COGIP-app/companies/update/$company[company_id]\"><button class='manage'><i class='fas fa-pen-square'></i></button><a>";
+                echo "<a href = \"/COGIP-app/companies/delete/$company[company_id]\"><button class='manage'><i class='fas fa-minus-square'></i></button><a>";
+            }
+            echo "</td></tr>";
 
        }
     ?>
-</table>   
+</table>
+
+<h2>Derniers contacts:</h2>
+<table>
+    <th>ID</th>
+    <th>Prénom</th>
+    <th>Nom</th>
+    <th>Email</th>
+    <th>Téléphone</th>
+    <th>Société</th>
+    <th>Gérer</th>
+    <?php
+       $contactsList = $model->data[2];
+       foreach ($contactsList as $contact) {
+            echo "<tr>";
+            echo "<td>".$contact['contact_id'].  "</td>";
+            echo "<td>".$contact['firstname'].  "</td>";
+            echo "<td>".$contact['lastname'].  "</td>";
+            echo "<td>".$contact['email'].  "</td>";
+            echo "<td>".$contact['phone'].  "</td>";
+            echo "<td>".$contact['name'].  "</td>";
+            echo "<td><a href = \"/COGIP-app/contacts/details/$contact[contact_id]\"><button class='manage'><i class='fas fa-external-link-square-alt'></i></button><a>";
+            if (Auth::isLogged() && $_SESSION['usertype'] === 'admin') {
+                echo "<a href = \"/COGIP-app/contacts/update/$contact[contact_id]\"><button class='manage'><i class='fas fa-pen-square'></i></button><a>";
+                echo "<a href = \"/COGIP-app/contacts/delete/$contact[contact_id]\"><button class='manage'><i class='fas fa-minus-square'></i></button><a>";
+            }
+            echo "</td></tr>";
+
+       }
+    ?>
+</table>
