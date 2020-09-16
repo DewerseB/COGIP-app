a5fe51631cb5e46f48028be06b78c31d23d134f4
Stephane <kalonjistephane@gmail.com>
2020-09-16 15:17:07 +0200

    correction 'option selected' des menus déroulants updates invoice et contact

--- view/pages/contacts/update.php
+++ view/pages/contacts/update.php
@@ -1,5 +1,5 @@
 <?php
-    $contactInfo = $model->data[0];
+    $contactInfo = $model->data[0][0];
     $companyList = $model->data[1][1];
 ?>
 
@@ -8,28 +8,30 @@
 <form class="contactForm" action="" method="post">
     <section class="firstname">
         <label for="firstname"><h2>Prénom</h2></label>
-        <input type="text" name="firstname" id="firstname" value="<?php echo $contactInfo[0]['firstname'] ?>">
+        <input type="text" name="firstname" id="firstname" value="<?php echo $contactInfo['firstname'] ?>">
     </section>
     <section class="lastname">
         <label for="lastname"><h2>Nom</h2></label>
-        <input type="text" name="lastname" id="lastname" value="<?php echo $contactInfo[0]['lastname'] ?>">
+        <input type="text" name="lastname" id="lastname" value="<?php echo $contactInfo['lastname'] ?>">
     </section>
     <section class="phone">
         <label for="phone"><h2>Téléphone</h2></label>
-        <input type="text" name="phone" id="phone" value="<?php echo $contactInfo[0]['phone'] ?>">
+        <input type="text" name="phone" id="phone" value="<?php echo $contactInfo['phone'] ?>">
     </section>
     <section class="email">
         <label for="email"><h2>Email</h2></label>
-        <input type="email" name="email" id="email" value="<?php echo $contactInfo[0]['email'] ?>">
+        <input type="email" name="email" id="email" value="<?php echo $contactInfo['email'] ?>">
     </section>
     <section class="company">
         <label for="company"><h2>Société</h2></label>
         <select class="custom-select" name="company" id="company" required>
-            <option selected value="Selectionnez your company">Selectionnez la société</option>
             <?php
                 foreach($companyList as $company){
             ?>
-                 <option value="<?php echo $company['company_id']; ?>"><?php echo $company['name'];?></option>
+            <option value="<?php echo $company['company_id']; ?>"<?php echo ($company['name'] == $contactInfo['name'])? "selected":""?>>
+                <?php echo $company['name'];?>
+            </option>
+                
             <?php
                 }
             ?>
--- view/pages/invoices/update.php
+++ view/pages/invoices/update.php
@@ -1,7 +1,8 @@
 <?php
-    $invoiceInfo = $model->data[0];
+    $invoiceInfo = $model->data[0][0];
     $contactList = $model->data[1][0];
     $companyList = $model->data[1][1];
+    var_dump($invoiceInfo);
  ?>
  
 <h1>Modifier la facture :</h1>
@@ -9,22 +10,23 @@
 <form class="invoiceForm" action="" method="post">
     <section class="invoice_number">
         <label for="invoice_number"><h2>Numéro de facture</h2></label>
-        <input type="text" name="invoice_number" id="invoice_number" value="<?php echo $invoiceInfo[0]['invoice_number'] ?>">
+        <input type="text" name="invoice_number" id="invoice_number" value="<?php echo $invoiceInfo['invoice_number'] ?>">
     </section>
 
     <section class="date">
         <label for="date"><h2>Date</h2></label>
-        <input type="date" name="date" id="date" value="<?php echo $invoiceInfo[0]['date'] ?>">
+        <input type="date" name="date" id="date" value="<?php echo $invoiceInfo['date'] ?>">
     </section>
 
     <section class="company">
         <label for="company"><h2>Société</h2></label>
         <select class="custom-select" name="company" id="company" required>
-            <option selected value="Selectionnez your company">Selectionnez la société</option>
             <?php
                 foreach($companyList as $company){
             ?>
-                 <option value="<?php echo $company['company_id']; ?>"><?php echo $company['name'];?></option>
+                    <option value="<?php echo $company['company_id']; ?>"<?php echo ($company['name'] == $invoiceInfo['name'])? "selected":""?>>
+                        <?php echo $company['name'];?>
+                    </option>
             <?php
                 }
             ?>
@@ -34,11 +36,13 @@
     <section class="contact">
         <label for="contact"><h2>Personne de contact pour la facture</h2></label>
         <select class="custom-select" name="contact" id="contact" required>
-            <option selected value="Selectionnez your contact">Selectionnez le contact</option>
             <?php
                 foreach($contactList as $contact){
             ?>
-                 <option value="<?php echo $contact['contact_id']; ?>"><?php echo $contact['lastname']. " ". $contact['firstname'];?></option>
+                <option value="<?php echo $contact['contact_id']; ?>"<?php echo ($contact['lastname'] == $invoiceInfo['lastname'] && $contact['firstname'] == $invoiceInfo['firstname'])? "selected":""?>>
+                        <?php echo $contact['lastname']. " ". $contact['firstname'];?>
+                </option>    
+                 <!-- <option value="<?php echo $contact['contact_id']; ?>"><?php echo $contact['lastname']. " ". $contact['firstname'];?></option> -->
             <?php
                 }
             ?>
