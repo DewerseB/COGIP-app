16cf34c349764eba1fa3eab4fd698634388bc553
MaxendreMartin <maxendremartin@gmail.com>
2020-09-17 14:52:42 +0200

    changement de header, css viré

--- view/header.php
+++ view/header.php
@@ -1,39 +1,46 @@
 <header>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark"> 
+        <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2"> 
+            <a class="navbar-brand" href="#"><img src="/COGIP-app/public/img/logo.png" alt="logo" width="30%"></a>
+        
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2"> 
+                <span class="navbar-toggler-icon"></span> 
+            </button> 
+        </div> 
     
+    
+        <div class="mx-auto order-0"> 
+            <ul class="navbar-nav mr-auto"> 
+                  <li class="nav-item "> 
+                      <a class="nav-link" href="/COGIP-app/dashboard">Accueil</a> 
+                  </li> 
+                  <li class="nav-item"> 
+                      <a class="nav-link" href="/COGIP-app/invoices/list">Factures</a> 
+                  </li> 
+                  <li class="nav-item"> 
+                      <a class="nav-link" href="/COGIP-app/companies/list">Sociétés</a> 
+                  </li> 
+                  <li class="nav-item"> 
+                      <a class="nav-link" href="/COGIP-app/contacts/list">Contacts</a> 
+                  </li>  
+            </ul> 
+        </div> 
 
-    <nav class="navbar navbar-expand-lg navbar-light  background-color: #c0c0c0;">
-  <a class="navbar-brand" href="#"><img src="/COGIP-app/public/img/logo.png" alt="logo" width="30%"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav navbar-right">
+        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2"> 
+            <ul class="navbar-nav ml-auto"> 
+                  <li class="nav-item"> 
+                    <?php
+                          if (Auth::isLogged()) {
+                              echo '<form action="" method="POST"><button class="nav" type="submit" name="submit" value="logout">Deconnexion</button></form>';
+                          } else {
+                              echo '<a href="/COGIP-app/login"><button class="nav" type="button">Connexion</button></a>';
+                          }
+                      ?>
+                  </li> 
+            </ul> 
 
-      <li class="nav-item ">
-        <a class="nav-link" href="/COGIP-app/dashboard">Accueil <span class="sr-only">(current)</span></a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="/COGIP-app/invoices/list">Factures</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="/COGIP-app/companies/list">Sociétés</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="/COGIP-app/contacts/list">Contacts</a>
-      </li>
-
-        </ul>
-        <?php
-            if (Auth::isLogged()) {
-                echo '<form action="" method="POST"><button class="nav" type="submit" name="submit" value="logout">Deconnexion</button></form>';
-            } else {
-                echo '<a href="/COGIP-app/login"><button class="nav" type="button">Connexion</button></a>';
-            }
-        ?>
-        </div>
-        </nav>
+        </div> 
+</nav> 
 
 </header>
